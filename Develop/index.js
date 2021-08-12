3613536340883cc41ee2d3cbac6cfba5effec84b
McHenry <lukemchenry311@gmail.com>
2021-08-12 00:18:57 -0600

    more question arrays

--- Develop/index.js
+++ Develop/index.js
@@ -1,44 +1,75 @@
 // TODO: Include packages needed for this application
+const fs = require ('fs');
+const markdown = require('./utils/generateMarkdown');
+const path = require('path');
 const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
+// TODO: make order follow readme-guide such as 
+// name, github name, project name, project description, technologies used,
+// github link, installation, instructions and examples, licenses, tests, emails
 function questions() {
     inquirer.prompt([
     {
         type: 'input',
-        name: 'title',
-        message: 'what is the title?'
+        name: 'name',
+        message: 'what is your name'
+    },
+    {
+        type: 'input',
+        name: 'gname',
+        message: 'what is your github username?'
     },
     {
         type: 'input',
         name: 'description',
-        message: 'description of the project'
+        message: 'description of your project'
+    },
+    {
+        type: 'checkbox',
+        name: 'technologies',
+        message: 'what languages did you use?',
+        choices: ['Javascript', 'HTML', 'CSS', 'Python']
     },
     {
         type: 'input',
-        name: 'motivation',
-        message: 'motivation for the project'
+        name: 'glink',
+        message: 'URL of the deployed application'
     },
     {
         type: 'input',
-        name: 'why',
-        message: 'why did you create this project'
+        name: 'installation',
+        message: 'how can the user install your project?'
     },
     {
         type: 'input',
-        name: 'problem',
-        message: 'what problem does your project solve'
+        name: 'instructions',
+        message: 'explain how to use the project'
+    },
+    // licenses, tests, emails
+    {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'what license would you like to use',
+        choices: ['MIT License', 'Apache 2.0', 'GNU v3.0']
     },
     {
         type: 'input',
-        name: 'learn',
-        message: 'what did you learn'
+        name: 'test',
+        message: 'what are your test instructions'
     },
     {
         type: 'input',
-        name: 'special',
-        message: 'what makes your project special'
+        name: 'email',
+        message: 'what is your email'
+    },
+    {
+        type: 'checkbox',
+        name: 'create',
+        message: 'what helped you create this project (if any)',
+        choices: ['collaborators', 'lessons', 'tutorials', 'none']
     }
-]) }
+])};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
